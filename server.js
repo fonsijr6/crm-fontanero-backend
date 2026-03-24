e7af9746cda10830261c2a8099bddbecb278754d
fonsijr6 <alfonsoondo97@gmail.com>
2026-03-24 15:09:02 +0100

    dominio vercel

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
 origin: [
     "http://localhost:8080",
     "http://localhost:5173",
-    "https://tu-frontend.vercel.app"
+    "https://fonsi-plumbflow-crm.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
