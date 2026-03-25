6a5cfb02c354232f1bb265af9bee5ad303021e99
fonsijr6 <alfonsoondo97@gmail.com>
2026-03-25 21:21:46 +0100

    se viene

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const invoiceController = require("../controllers/invoice.controller");
+const { auth } = require("../middleware/auth.mw");
 
 // ✅ Todas
 router.get("/", invoiceController.getInvoices);
